ad86c2416bb93c3d05acfe72739812fd9383d5a5
MarcTzh <marcustan26@hotmail.com>
2022-09-02 15:08:53 +0800

    "add NavBar code and install bcrypt"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
-import SignupPage from './pages/SignupPage';
+import SignupPage from './components/SignupPage';
 import {Box} from "@mui/material";
 
 function App() {
--- user-service/controller/user-controller.js
+++ user-service/controller/user-controller.js
@@ -19,3 +19,22 @@ export async function createUser(req, res) {
         return res.status(500).json({message: 'Database failure when creating new user!'})
     }
 }
+
+function saltPassword(password) {
+    const bcrypt = require("bcryptjs")
+    const saltRounds = 10
+    bcrypt.genSalt(saltRounds, (saltError, salt) => {
+      if (saltError) {
+        throw saltError
+      } else {
+        bcrypt.hash(password, salt, function(hashError, hash) {
+          if (hashError) {
+            throw hashError
+          } else {
+            console.log(hash)
+            return hash
+          }
+        })
+      }
+    })
+}
