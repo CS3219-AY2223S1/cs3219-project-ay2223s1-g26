8ee08966ea2763894d3c81037443fbccbc1e84b9
Tan Eu Zin <taneuzin@u.nus.edu>
2022-09-08 14:32:11 +0800

    feat: initialises basic collab-service

--- collab-service/index.js
+++ collab-service/index.js
@@ -1,34 +1,26 @@
-let express = require("express");
-let socket = require("socket.io");
-const { io } = require("socket.io-client");
+const { createServer } = require("http");
+const express = require("express");
+const { Server } = require("socket.io");
+const app = express();
 
-let app = express();
+const httpServer = createServer(app);
 
-let server = app.listen(8080);
-console.log("Server listening on port 8080");
+const io = new Server(httpServer, { cors: { origin: "*" } });
 
-app.use(express.static("public"));
+io.on("connection", (socket) => {
+  // socket.on("private message", (targetSocket, msg) => {
+  //   socket.to(targetSocket).emit("private message", socket.id, msg);
+  // });
+  socket.on("match", (args) => {
+    console.log(args);
+    socket.on(args.USER_ID, (msg) => {
+      io.emit(args.USER_ID, msg);
+      console.log("msg: ", msg);
+    });
+  });
 
-const backIO = socket(server, {
-  cors: { origin: "*" },
+  console.log("connection: ", socket.id);
+  return;
 });
 
-connection = (socket) => {
-  handleTextSent = () => {
-    text.text = data.text;
-    backIO.sockets.emit("text", data);
-    return;
-  };
-  receiveTextChange = (text) => {
-    socket.emit("emitttedTextChange");
-  };
-  console.log("socket arg: ", socket.arg);
-  console.log("a new user with id " + socket.id + " has entered");
-  socket.emit("newUser", text);
-  socket.on("text", handleTextSent);
-  socket.on("join", (args) => console.log("hello: ", args));
-  socket.on("textChange", receiveTextChange);
-  return;
-};
-backIO.sockets.on("connect", connection);
-let text = "";
+httpServer.listen(8080);
