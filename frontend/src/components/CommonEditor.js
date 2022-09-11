b278aad609c1b376b764882fd3dd930efeae6682
Tan Eu Zin <taneuzin@u.nus.edu>
2022-09-11 23:30:44 +0800

    Implement basic code editor

--- frontend/src/components/CommonEditor.js
+++ frontend/src/components/CommonEditor.js
@@ -1,29 +1,47 @@
 import * as React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 const { io } = require("socket.io-client");
 
-const USER_ID =
-  process.env.REACT_APP_ENV === "dev_1"
-    ? process.env.REACT_APP_USER1
-    : process.env.REACT_APP_ENV === "dev_2"
-    ? process.env.REACT_APP_USER2
-    : "UNDEFINED";
+// Only for dev environment, to simulate 2 clients
+let USER_ID;
+let PARTNER_ID;
+
+if (process.env.REACT_APP_ENV === "dev_1") {
+  USER_ID = process.env.REACT_APP_USER1;
+  PARTNER_ID = process.env.REACT_APP_USER2;
+} else if (process.env.REACT_APP_ENV === "dev_2") {
+  USER_ID = process.env.REACT_APP_USER2;
+  PARTNER_ID = process.env.REACT_APP_USER1;
+}
 
 const CommonEditor = () => {
   const [textValue, setTextValue] = useState("");
-  const clientSocket = io("http://localhost:8080");
-  clientSocket.emit("join", USER_ID);
-  clientSocket.on("emittedTextChange", receiveTextChange);
+  const [clientSocket, setClientSocket] = useState();
+  useEffect(() => {
+    let socket = io("http://localhost:8080");
+    socket.on("connect", () => {
+      console.log(socket.id);
+      socket.emit("match", { USER_ID, PARTNER_ID });
+    });
+    socket.on(PARTNER_ID, handleChangeReceived);
+    socket.on("confirmed", console.log("confirmed"));
+    setClientSocket(socket);
+  }, []);
 
-  const handleChange = (text) => {
-    setTextValue(text);
-    clientSocket.emit("textChange", text);
-    console.log(text.target.value);
+  const handleChangeEmitted = (text) => {
+    console.log("change emitted: ", text.target.value);
+    console.log(
+      "caret position: ",
+      document.getElementById("editor").selectionEnd
+    );
+    setTextValue(text.target.value);
+    clientSocket.emit(USER_ID, text.target.value);
   };
 
-  const receiveTextChange = (text) => {
+  const handleChangeReceived = (text) => {
+    console.log("received: ", text);
     setTextValue(text);
   };
 
@@ -38,12 +56,12 @@ const CommonEditor = () => {
     >
       <div>
         <TextField
-          id="outlined-multiline-static"
+          id="editor"
           label="Multiline"
           multiline
           rows={4}
           defaultValue="Default Value"
-          onChange={handleChange}
+          onInput={handleChangeEmitted}
           value={textValue}
         />
       </div>
