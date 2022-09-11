7dd4567b2edeee5ea807b55dd2941804f963d371
MarcTzh <marcustan26@hotmail.com>
2022-09-11 14:16:40 +0800

    add working user service route for firebase

---
+++ user-service/functions/api/user/route.js
@@ -0,0 +1,55 @@
+const functions = require('firebase-functions')
+var auth = require('../../helper/auth')
+
+async function onboard(admin, res, uid, name, email) {
+  const userAccount = {
+    name,
+    email,
+    updatedAt: admin.firestore.FieldValue.serverTimestamp(),
+  }
+  try {
+    const response = await admin.firestore().collection('users').doc(uid)
+      .set(userAccount, { merge: true })
+    if (response) {
+      functions.logger.log('onboard success', uid)  
+    }
+  } catch (e) {
+    functions.logger.error(e)
+    res.status(500).send(false)
+    return
+  }
+  res.status(200).send(userAccount)
+  return userAccount
+}
+
+module.exports = {
+  _404(req, res) {
+    res.status(404).send({
+      status: 'not_found'
+    })
+  },
+  async post(req, res, admin) {
+    if (req.params.route === 'test') {
+      res.sendStatus(200)
+      return
+    }
+    const uid = await auth.getUserUId(req, res)
+    if (!uid) {
+      return
+      // change to set (can change a few not all values -- expandable)
+    } else if (req.params.route === 'onboard') {
+      if ((req.body.name, req.body.email)) {
+        await onboard(admin, res, uid, req.body.name, req.body.email)
+        return
+      } else {
+        res.status(400).send({
+          status: 'invalid_body'
+        })
+        return
+      }
+    } else {
+      res.status(404).send('Not found')
+      return
+    }
+  }
+}
--- user-service/functions/index.js
+++ user-service/functions/index.js
@@ -1,34 +1,8 @@
-import functions from 'firebase-functions'
-import serviceAccount from '../.env'
+const admin = require('firebase-admin')
+const functions = require('firebase-functions')
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
-});
-
-const rawBodySaver = function (req, res, buf, encoding) {
-  if (buf && buf.length) {
-    req.rawBody = buf.toString(encoding || 'utf8')
-  }
+if (!admin.apps.length) {
+  admin.initializeApp()
 }
 
-/*
- * Parse application/x-www-form-urlencoded && application/json
- */
-const app = express()
-app.disable('x-powered-by')
-app.use(bodyParser.json())
-app.use(cors)
-app.use(bodyParser.urlencoded({ verify: rawBodySaver, extended: true }));
-app.use(bodyParser.json({ verify: rawBodySaver }))
-
-app.post('/api/oauth/:route(*)', (req, res) => {
-  require('./oauth/route').post(req, res, admin)
-})
-app.get('*', require('./routes')._404Html)
-app.post('*', require('./routes')._404)
-
-module.exports = app
+exports.api = functions.https.onRequest(require('./api/index'))
