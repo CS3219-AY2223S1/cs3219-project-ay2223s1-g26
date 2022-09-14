7caac9552da25dfaba26303a1dca7016ccd47974
MarcTzh <marcustan26@hotmail.com>
2022-09-14 11:54:44 +0800

    update login page

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,15 +1,51 @@
 import React, { useEffect, useState } from "react";
 import { signInWithGoogle } from "../firebase";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Avatar from '@mui/material/Avatar';
+import googleImage from '../images/google.png';
+import codingImage from '../images/coding.jpg';
 
 import "./Login.css";
 function Login() {
   return (
+    // <div className="login">
+    //   <div className="login__container">
+    //     <button className="login__btn login__google" onClick={signInWithGoogle}>
+    //       Login with Google
+    //     </button>
+    //   </div>
+    // </div>
     <div className="login">
-      <div className="login__container">
-        <button className="login__btn login__google" onClick={signInWithGoogle}>
-          Login with Google
-        </button>
-      </div>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          component="img"
+          height="140"
+          image={codingImage}
+          alt="codingImage"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Log in to PeerPrep
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions style={{justifyContent: 'center'}}>
+          <Button
+            variant="contained"
+            onClick={signInWithGoogle}
+            startIcon={<Avatar src={googleImage}/>}>
+              Sign in with Google
+          </Button>
+        </CardActions>
+      </Card>
     </div>
   );
 }
