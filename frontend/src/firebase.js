cd001d64ba57dd1ed3db78ea45a16e11374f9914
MarcTzh <marcustan26@hotmail.com>
2022-10-01 12:17:34 +0800

    create new user in database

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -11,23 +11,16 @@ import {
 } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
 
-const { firebaseConfig, backendApi } = require('./.env.json')
+const { firebaseConfig } = require('./.env.json')
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
-const axios = require('axios')
 
 const googleProvider = new GoogleAuthProvider();
 const signInWithGoogle = async () => {
   try {
     const result = await signInWithPopup(auth, googleProvider);
     if (result && result.user) {
-      const idToken = await result.user.getIdToken()
-      // const userResponse = await axios.post(`${backendApi}/user/getUser`, {
-      //   headers: {
-      //     Authorization: 'Bearer ' + idToken
-      //   }
-      // })
       return true
     }
   } catch (err) {
--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,8 +1,29 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { context } from '../context';
 
 function Dashboard() {
-  const { user, setIsLoading } = useContext(context)
+  const { user, setIsLoading, $axios } = useContext(context)
+  useEffect(() => {
+    getDashboardInfo()
+  }, [user])
+
+  async function getDashboardInfo() {
+    console.log(user)
+    if (!user) {
+      return
+    }
+    // setIsLoading(true)
+    try {
+      const response = await $axios.post(`${$axios.defaults.baseURL}/getUser`)
+      const data = response.data
+      if (!response) {
+        // setMessage('An error occurred')
+        return
+      }
+    } catch (e) {
+      console.log(e)
+    }
+  }
 
   return (
     <div>
